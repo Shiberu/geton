6cd970b32eebc759d64acd714633c22c41f9b06d
Kevin Chen <kvchen@berkeley.edu>
2014-11-30 21:11:51 -0800

    Add create function to game controller

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,4 +2,12 @@ class GamesController < ApplicationController
   def new
     @game = Game.new
   end
+
+  def create
+    @game = Game.create(game_params)
+  end
+
+  def game_params
+    params.require(:game).permit(:name, :icon, :banner)
+  end
 end
